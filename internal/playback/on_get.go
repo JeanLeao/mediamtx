c556fded4856d0cfcb75f1ca1ebd4b73361acae1
jeanc.leaosantos <jeanc.leaosantos@gmail.com>
2024-08-09 19:14:56 -0300

    correção

--- internal/playback/on_get.go
+++ internal/playback/on_get.go
@@ -1,8 +1,10 @@
 package playback
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
@@ -18,6 +20,12 @@ import (
 type writerWrapper struct {
 	ctx     *gin.Context
 	written bool
+}
+
+type Recording struct {
+	Path     string  `json:"path"`
+	Duration float64 `json:"duration"`
+	Start    string  `json:"start"`
 }
 
 func (w *writerWrapper) Write(p []byte) (int, error) {
@@ -56,14 +64,19 @@ func seekAndMux(
 		}
 		defer f.Close()
 
+		fmt.Printf("Segmento ENCONTRADO: %+v\n", *f)
 		firstInit, err = segmentFMP4ReadInit(f)
 		if err != nil {
 			return err
 		}
 
+		fmt.Printf("firstInit: %+v\n", *firstInit)
+
 		m.writeInit(firstInit)
 
 		segmentStartOffset := start.Sub(segments[0].Start)
+
+		fmt.Printf("segmentStartOffset: %+v\n", segmentStartOffset)
 
 		segmentMaxElapsed, err := segmentFMP4SeekAndMuxParts(f, segmentStartOffset, duration, firstInit, m)
 		if err != nil {
@@ -152,15 +165,84 @@ func (p *Server) onGet(ctx *gin.Context) {
 		return
 	}
 
-	segments, err := findSegmentsInTimespan(pathConf, pathName, start, duration)
+	// segments, err := findSegmentsInTimespan(pathConf, pathName, start, duration)
+	// if err != nil {
+	// 	if errors.Is(err, errNoSegmentsFound) {
+	// 		p.writeError(ctx, http.StatusNotFound, err)
+	// 	} else {
+	// 		p.writeError(ctx, http.StatusBadRequest, err)
+	// 	}
+	// 	return
+	// }
+
+	url := fmt.Sprintf("http://127.0.0.1:4549/api/v1/recording/list-without-concatenation?device=%s&start=%s&duration=%s", pathName, ctx.Query("start"), ctx.Query("duration"))
+	fmt.Println("URL:", url)
+	resp, err := http.Get(url)
 	if err != nil {
-		if errors.Is(err, errNoSegmentsFound) {
-			p.writeError(ctx, http.StatusNotFound, err)
-		} else {
-			p.writeError(ctx, http.StatusBadRequest, err)
-		}
+		fmt.Println("Erro ao fazer a requisição:", err)
 		return
 	}
+	defer resp.Body.Close()
+
+	// Lendo o corpo da resposta
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erro ao ler o corpo da resposta:", err)
+		return
+	}
+
+	// Deserializando o corpo da resposta para um slice de Recording
+	var recordings []Recording
+	err = json.Unmarshal(body, &recordings)
+	if err != nil {
+		fmt.Println("Erro ao deserializar o JSON:", err)
+		return
+	}
+
+	// Check if recordings is empty
+	if len(recordings) == 0 {
+		fmt.Println("Nenhuma gravação encontrada.")
+		return
+	}
+
+	// Imprimindo o JSON deserializado
+	for _, recording := range recordings {
+		fmt.Printf("Path: %s\nStart: %s\n", recording.Path, recording.Start)
+	}
+
+	segments := make([]*Segment, len(recordings))
+	for i, recording := range recordings {
+
+		parsedTime, err := time.Parse(time.RFC3339, recording.Start)
+		if err != nil {
+			fmt.Println("Erro ao converter a string para time.Time:", err)
+			return
+		}
+
+		year, month, day := parsedTime.Date()
+		hour, min, sec := parsedTime.Clock()
+		nsec := parsedTime.Nanosecond()
+
+		segments[i] = &Segment{
+			Fpath: recording.Path,
+			Start: time.Date(year, month, day, hour, min, sec, nsec, time.UTC),
+		}
+	}
+
+	fmt.Println("Segmentos encontrados:")
+	for i, segment := range segments {
+		fmt.Printf("Segmento %d: %+v\n", i, *segment)
+	}
+
+	// local := "/home/jean/Documentos/mediamtx/mediamtx"
+
+	// Fpath string
+	// Start time.Time
+	// "2024-08-08 17:13:29.437452 -0300 -03"
+	// 2024-08-08_17-42-02-527827.mp4
+	// foo := []*Segment{
+	// 	{Fpath: "/home/jean/Documentos/mediamtx/mediamtx/recordings/66abfd2b96c48cd25ab58604/2024-08-08_17-13-29-437452.mp4", Start: time.Date(2024, 8, 8, 17, 42, 02, 527827, time.FixedZone("UTC-3", -3*60*60))},
+	// }
 
 	err = seekAndMux(pathConf.RecordFormat, segments, start, duration, m)
 	if err != nil {
